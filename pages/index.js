e7b7dcf9bbc16f484246849ef8629e3f2a384cdb
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-11-09 19:17:41 -0300

    setup inicial

---
+++ pages/index.js
@@ -0,0 +1,88 @@
+import config from "../config.json"
+import styled from "styled-components"
+
+function HomePage() {
+    const estiloDaHomePage ={ backgroundColor: "red"}
+
+    return (
+        <div style={estiloDaHomePage}>
+            <Menu/>
+            <Header />
+            <Timeline playlists={config.playlists}>
+                conteudo:
+            </Timeline>
+        </div>
+    )
+  }
+  
+  export default HomePage
+
+  function Menu(){
+    return(
+        <div>
+            Menu
+        </div>
+    )
+  }
+
+  const StyledHeader = styled.div`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius:50%;
+    }
+    .user-info{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+  `;
+  function Header(){
+    return(
+        <StyledHeader>
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>{config.name}</h2>
+                    <p>{config.Job}</p>
+                </div>
+               
+            </section>
+        </StyledHeader>
+    )
+  }
+
+  function Timeline(props){
+
+    const playlistNames = Object.keys(props.playlists);
+
+    return(
+        <div>
+            {playlistNames.map((playlistName) => {
+                const videos = props.playlists[playlistName];
+
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                           {
+                            videos.map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img  src={video.thumb}/>
+                                        <span>
+                                            {video.title}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </section>
+                )
+            })}
+            
+        </div>
+    )
+  }
